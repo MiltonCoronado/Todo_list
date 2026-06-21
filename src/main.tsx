cea8c991fe7cdb99a0e86a33f77f824558268b2e
MiltonCoronado <miltonext@hotmail.com>
2026-06-21 02:15:22 -0500

    chore: rename main file and update imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { TasksList } from './TaskList.tsx';
+import { TodoList } from './TodoList.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TasksList />
+    <TodoList />
   </StrictMode>,
 );
